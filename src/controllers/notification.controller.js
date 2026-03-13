931d207327c2c6a79e13b573e0fd58c916c259b9
hena-chicmic <hena@chicmicstudios.in>
2026-03-13 17:51:54 +0530

    notification update

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,3 +1,15 @@
 const notificationService=require('../services/notification.service')
 
-const getNotifications=async(req)
+const getNotifications=async(req,res,next)=>{
+    try{
+        const userId=req.user.userId
+        const notification_id=req.params
+        const notification=await notificationService.getNotifications(notification_id)
+        res.status(200).json({
+            success:true,
+
+        })
+    }catch(error){
+        next(error)
+    }
+}
