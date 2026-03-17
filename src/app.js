efb69afa31f60c11c971c717dad47b459ce09d2d
Vadde Dhanush <vadde.dhanush@chicmicstudios.in>
2026-03-17 10:40:38 +0530

    syntax fixes

--- src/app.js
+++ src/app.js
@@ -9,14 +9,10 @@ const authRoutes = require('./routes/auth.routes');
 const discoveryRoutes = require('./routes/discovery.routes');
 const matchRoutes = require('./routes/match.routes');
 const interactionRoutes = require('./routes/interaction.routes');
-<<<<<<< HEAD
 const messageRoutes = require('./routes/message.routes');
-const notificationRoutes = require('./routes/notification.routes');
-
-=======
+const notificationRoutes = require('./routes/notification.routes')
 const userRoutes = require('./routes/user.routes');
-//const notificationRoutes = require('./routes/notification.routes');
->>>>>>> 7cf74c9 (swagger and media upload done)
+
 const app = express();
 
 app.use(cors({
@@ -35,14 +31,10 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/discovery', discoveryRoutes);
 app.use('/api/v1/matches', matchRoutes);
 app.use('/api/v1/interactions', interactionRoutes);
-<<<<<<< HEAD
 app.use('/api/v1/messages', messageRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
-
-=======
 app.use('/api/v1/users', userRoutes);
 //app.use('/api/v1/notifications', notificationRoutes);
->>>>>>> 7cf74c9 (swagger and media upload done)
 // centralized error handler
 app.use(errorHandler);
 
